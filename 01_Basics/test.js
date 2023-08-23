cccf4d3621a546d157648bfd44ab91c49df346c2
Mr Mic <127715071+brohit22@users.noreply.github.com>
2023-08-23 14:42:29 +0000

    datatypes

--- 01_Basics/test.js
+++ 01_Basics/test.js
@@ -1,6 +1,6 @@
 console.log("Basics of Javascript");
 
-//-----  01- Let, Const , Var   ----//
+//-----  01. Variables: Let, Const , Var    ----//
 const userID = 100
 let userName = "Tony"
 var userEmail = "tony@gmail.com"
@@ -17,3 +17,24 @@ console.table([userID, userName, userEmail, userCity])
 Prefer to not use var 
 because of issue in block scope and functional scope. 
 */
+
+//////////////////////////////////////////////////////////////////////////////
+
+//---- 02. Datatypes ----//
+
+/* Primitive 
+  number => 2^53
+  bigInt 
+  string => ""
+  boolean => true || false
+  undefined 
+  null
+  symbol => unique
+*/
+
+// Object
+
+console.log(typeof undefined); // undefined 
+console.log(typeof null); // object
+
+/////////////////////////////////////////////////////////////////////////////
