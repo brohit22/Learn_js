3dffb108479c0149ef244aa376b56933e362c4a6
Mr Mic <127715071+brohit22@users.noreply.github.com>
2023-09-15 13:35:10 +0000

    conditionals completed

---
+++ 01_Basics/conditonal.js
@@ -0,0 +1,25 @@
+// if..
+if( age > 18){ console.log("Your can drive car");}
+
+// if .. else ..
+if(age > 18){
+    console.log("Your can drive car");
+}else {
+    console.log("You are not able to drive");
+}
+
+// if .. elseif..
+
+if(age > 18){
+    console.log("You are Adult now");
+}else if(age == 14){
+    console.log("Your are Teen now");
+}else{
+    console.log("Your are child");
+}
+
+// ? operator 
+
+let ans = age > 18 ? "adult" : "child";
+
+
---
+++ Challenges/code1.js
@@ -0,0 +1,3 @@
+// Write a js code that will return random letter from your Name
+const name = "Godzilla"
+console.log(name.charAt(Math.floor(Math.random()*name.length)));
