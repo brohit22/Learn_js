c45d87afca1cca2f7311c433b44894bd65906a08
Mr Mic <127715071+brohit22@users.noreply.github.com>
2023-09-15 12:52:12 +0000

    Maths properties and methods learned

---
+++ 01_Basics/math.js
@@ -0,0 +1,32 @@
+
+// The Math.exp() static method returns e raised to the power of a number. That is
+// 𝙼𝚊𝚝𝚑.𝚎𝚡𝚙(𝚡)=e.pow(x);
+// console.log(Math.exp(5)); // output : 148.4131591025766
+
+// The Math.ceil() static method always rounds up and returns the smallest integer greater than or equal to a given number.
+// console.log(Math.ceil(5.4));  // output : 6
+
+// The Math.floor() static method always rounds down and returns the largest integer less than or equal to a given number.
+// console.log(Math.floor(5.4));  // output : 5
+
+//The Math.hypot() static method returns the square root of the sum of squares of its arguments. That is,
+// Math.hypot(a,b,c,...n) = sqrt(sqr(a) + sqr(b) + sqr(c)+.......sqr(n));
+// console.log(Math.hypot(3,4));   // output : 5
+// console.log(Math.hypot(2,3,4)); // output : 5.385164807134504
+
+// The Math.imul() static method returns the result of the C-like 32-bit multiplication of the two parameters.
+// console.log(Math.imul(-5,12));  // output : -60
+
+// The Math.max() static method returns the largest of the numbers given as input parameters, or -Infinity if there are no parameters.
+// console.log(Math.max(-1, -3, -2)); // output: -1
+
+// The Math.min() static method returns the smallest of the numbers given as input parameters, or Infinity if there are no parameters.
+// console.log(Math.min(-1, -3, -2)); // output: -3
+
+// The Math.pow() static method returns the value of a base raised to a power. That is
+// console.log(Math.pow(2,3));
+
+console.log(Math.round(Math.random()*10 + 1));
+
+// The Math.trunc() static method returns the integer part of a number by removing any fractional digits.
+console.log(Math.trunc(42.84)); // output: 42
